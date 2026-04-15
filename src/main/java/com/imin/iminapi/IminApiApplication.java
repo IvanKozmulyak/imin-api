8abb78bcffa115726583f3d1523241512a074dbb
ikozmuliak <ikozmuliak@mercans.com>
2026-04-15 18:11:59 +0200

    Init commit

---
+++ src/main/java/com/imin/iminapi/IminApiApplication.java
@@ -0,0 +1,13 @@
+package com.imin.iminapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IminApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IminApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/imin/iminapi/IminApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imin.iminapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IminApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
